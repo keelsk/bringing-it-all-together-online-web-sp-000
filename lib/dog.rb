ed52c840b08a6448580f69c98b1eefb179719024
Kirsten Keels <keelsk@gmail.com>
2020-03-19 22:27:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -91,5 +91,9 @@ class Dog
     end.last
   end
   
-  
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
 end
