1476d76220a304cee3473abd6c09d0191bd6a529
Kirsten Keels <keelsk@gmail.com>
2020-03-19 21:56:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -42,8 +42,8 @@ class Dog
     end
   end
   
-  def self.create(name:, breed:)
-    dog = self.new(name, breed)
+  def self.create(name, breed)
+    dog = self.new(name: name, breed: breed)
     dog.save
     dog
   end
