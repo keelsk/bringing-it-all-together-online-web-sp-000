d76ef17aa04af89bad8ba1bb06b071a33e1cfb1a
Kirsten Keels <keelsk@gmail.com>
2020-03-19 22:16:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -72,7 +72,8 @@ class Dog
     if !dog.empty?
       binding.pry
       dog_data = dog[0]
-      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+      dog = Dog.new(name: dog_data[1], breed: dog_data[2])
+      dog.id = dog_data[0]
     else
       dog = self.create(name: name, breed: breed)
     end
